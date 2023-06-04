b496e9609bd9f609973a60cc41d680a49bd32c85
Abhay Pandey <abhay_pandey@persistent.com>
2023-06-04 10:38:05 +0530

    DSA

--- binaryTree/basic.js
+++ binaryTree/basic.js
@@ -68,47 +68,4 @@ const depthFirstValueRec = (root) => {
 
     return [root.val, ...left, ...right];
 }
-
-var data = [
-    {
-        img :'https://cdn3.mydukaan.io/app/image/700x700/?url=https://dukaan-us.s3.amazonaws.com/6963100/96a02c56-db7d-4dd4-a1c3-9cd0850e0181/frame-68-acd8f358-b66a-490c-973f-8cc4251dbd06-11c2e911-759d-4950-bdab-5c6edf7aad1d.png',
-        id:'1',
-        price:'90',
-        title: 'Classic Dress',
-        strikePrice:'120',
-        off: '25'
-    },
-    {
-        img :'https://cdn3.mydukaan.io/app/image/700x700/?url=https://dukaan-us.s3.amazonaws.com/6963100/96a02c56-db7d-4dd4-a1c3-9cd0850e0181/frame-79-6ffc3037-7dbc-4283-b363-7e65004e5421-3c99e223-6851-488d-ba46-e6ba48fdca6f.png',
-        id:'2',
-        price:'120',
-        title: 'Indian Dress',
-        strikePrice:'320',
-        off: '63'
-    },
-    {
-        img :'https://cdn3.mydukaan.io/app/image/700x700/?url=https://dukaan-us.s3.amazonaws.com/6963100/96a02c56-db7d-4dd4-a1c3-9cd0850e0181/frame-67-db68d0d9-51f1-4c28-a144-d15f97667e24-a07518ce-c94e-4da3-94d2-617b8a6751fa.png',
-        id:'3',
-        price:'120',
-        title: 'Lehenga',
-        strikePrice:'320',
-        off: '60'
-    }
-]
-
-for(let first of data) {
-    console.log(first);
-}
-
-
 // console.log(depthFirstValueRec(a));
-
-test(2)
-test3();
-function test(a){
-    console.log(a);
-}
-
-var test3 = function(b){
-    console.log(b)
-}
---
+++ stack&Queue/basic.js
@@ -0,0 +1,53 @@
+// stack for example :- stack overflow
+//LIFO : last out first
+// First inserted last out
+// Insert on the top 
+// pop and Push
+// Queue : first in first out
+// FIFO : first in first out
+// 
+
+
+class CustomStack {
+    constructor(){
+        this.items = [];
+    }
+
+    push(val){
+        this.items.push(val);
+    }
+
+    pop(){
+        return this.items.pop();
+    }
+
+    peek(){
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty(){
+        return this.items.length === 0;
+    }
+
+    size(){
+        return this.items.length;
+    }
+
+    print(){
+        console.log(this.items.toString());
+    }
+}
+
+const st = new CustomStack();
+console.log(st.isEmpty());
+
+st.push(23);
+st.push(43);
+st.pop();
+st.print();
+
+st.push(55);
+st.push(76);
+console.log(st.peek());
+console.log(st.print());
+
